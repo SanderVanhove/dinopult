ac07a975ff36111b70acc0e1b60b7906d22563cd
Lamasaurus <sandervhove@gmail.com>
2021-09-05 12:37:40 +0200

    Add splash and menu screens

---
+++ src/Screens/Menu/Menu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+func _on_Label_gui_input(event):
+	if not event as InputEventMouseButton or event.pressed:
+		return
+	
+	OS.shell_open("https://twitter.com/SanderVhove")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Screens/Level/Level.tscn")
+
+
+func _on_Label2_gui_input(event):
+	if not event as InputEventMouseButton or event.pressed:
+		return
+	
+	OS.shell_open("https://www.patreon.com/sandervanhove")
---
+++ src/Screens/Splash/Splash.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+onready var logo: TextureRect = $Control/CenterContainer/TextureRect
+onready var tween: Tween = $Tween
+
+
+func _ready():
+	tween.interpolate_property(logo, "modulate:a", 0, 1, .5, Tween.TRANS_SINE, Tween.EASE_OUT_IN)
+	tween.interpolate_property(logo, "modulate:a", 1, 0, .5, Tween.TRANS_SINE, Tween.EASE_OUT_IN, 1.5)
+	tween.start()
+	
+	yield(tween, "tween_all_completed")
+	get_tree().change_scene("res://Screens/Menu/Menu.tscn")
